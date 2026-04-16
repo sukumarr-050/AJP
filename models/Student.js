b517aed47caba3864b4c76f9ea9c63951558ffd4
Kancharla Sukumar <kancharlasukumar008@gmail.com>
2026-04-16 15:26:20 +0530

    Delete models directory

--- models/Student.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-const StudentSchema=new  mongoose.Schema({
-    name:String,
-    age: Number,
-    Course: String
-});
-module.exports=mongoose.model("student",(StudentSchema));
